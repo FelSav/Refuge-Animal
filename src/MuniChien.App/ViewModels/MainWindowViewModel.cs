1217731192a4e82dbb890f4b8f8d0eaff9e59b09
FelSav <felix.savard03@gmail.com>
2026-09-23 10:30:40 -0400

    mettre en place la fondation MVVM
    Ajoute les ViewModels de base pour l’accueil et la recherche.
    
    Configure MainWindowViewModel, les DataTemplates et le ContentControl afin d’afficher automatiquement la vue correspondant au ViewModel actif.

---
+++ src/MuniChien.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace MuniChien.App.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private ViewModelBase _currentViewModel;
+
+    public MainWindowViewModel()
+    {
+        _currentViewModel = new HomeViewModel();
+    }
+
+    public ViewModelBase CurrentViewModel
+    {
+        get => _currentViewModel;
+
+        set
+        {
+            _currentViewModel = value;
+            OnPropertyChanged();
+        }
+    }
+}
